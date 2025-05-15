95b2e8d65e254c8fa291b3b906eed486f90e0ec9
HiHighs <degroote.jonas@hotmail.com>
2025-05-15 11:35:43 +0200

    refactored images

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -2,21 +2,23 @@ import React, { useState } from "react";
 import styles from './Menu.module.css';
 import logo from '../assets/logo-circle.png';
 import logoSmile from '../assets/logo-circle-smile.png';
-import instagram from '../assets/instagram.png'
+import instagram from '../assets/instagram.png';
 import { NavLink } from 'react-router-dom';
 import MenuItem from '../Components/MenuItem';
-
 
 import '@fontsource/vt323';
 
 function Menu() {
   const [isHovered, setIsHovered] = useState(false);
 
+  const getLinkClass = ({ isActive }) =>
+    `${styles.link} ${isActive ? styles.activeLink : ''}`;
+
   return (
     <div className={styles.menu}>
 
       <div className={styles.logo}>
-      <NavLink to='/'>
+        <NavLink to='/'>
           <img
             src={isHovered ? logoSmile : logo}
             alt="Logo"
@@ -28,31 +30,34 @@ function Menu() {
 
       <MenuItem title="Portfolio" >
         <MenuItem title="Paintings">          
-            <NavLink to='paintings_2024' className={styles.link}>
-              <MenuItem title='2024'/>
-            </NavLink>          
-            <NavLink to='paintings_2025' className={styles.link}>
-              <MenuItem title='2025'/>
-            </NavLink>
+          <NavLink to='paintings_2024' className={getLinkClass}>
+            <MenuItem title='2024' />
+          </NavLink>          
+          <NavLink to='paintings_2025' className={getLinkClass}>
+            <MenuItem title='2025' />
+          </NavLink>
         </MenuItem>
         
-        
         <MenuItem title="Drawings">          
-          <NavLink to='drawings_2024' className={styles.link}>
-            <MenuItem title='2024'/>
+          <NavLink to='drawings_2024' className={getLinkClass}>
+            <MenuItem title='2024' />
           </NavLink>          
-          <NavLink to='drawings_2025' className={styles.link}>
-            <MenuItem title='2025'/>
+          <NavLink to='drawings_2025' className={getLinkClass}>
+            <MenuItem title='2025' />
           </NavLink>
         </MenuItem>
       </MenuItem>
       
-      <NavLink to='/about' className={styles.link}>
+      <NavLink to='/about' className={getLinkClass}>
         <MenuItem title="About" />
       </NavLink>
 
       <div className={styles.bottomIcon}>
-        <NavLink to='https://www.instagram.com/jonasdrawscats' target="_blank" rel="noopener noreferrer">
+        <NavLink
+          to='https://www.instagram.com/jonasdrawscats'
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img src={instagram} alt='Instagram' className={styles.instagram} />
         </NavLink>
       </div>
