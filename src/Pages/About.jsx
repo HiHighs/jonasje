3f00b9e32d099861ae088ebf52409417e3ad6f07
HiHighs <degroote.jonas@hotmail.com>
2025-05-22 15:21:00 +0200

    Updated about page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -7,38 +7,21 @@ function About() {
             <img src={img} className={styles.img} />
             <h1>ABOUT ME</h1>
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Etiam a posuere neque.
-                Pellentesque vitae justo nec nisl ultricies varius.
-                Mauris convallis justo sed lobortis sollicitudin.
-                Nunc lobortis turpis dolor, vitae ultricies nibh varius vel.
-                Phasellus non aliquam dolor. In vehicula eros lectus, nec blandit tortor lobortis vel.
-                Suspendisse at aliquet eros.
+                Hi! I'm Jonas, an artist based in Leuven, Belgium.
+                I love working with ink and watercolor to create random things.
+                You can explore a selection of my creations under the "Portfolio" tab.
             </p>
             <p>
-                Quisque lacinia egestas justo quis interdum.
-                Morbi faucibus dui sed ligula interdum, ac varius nisl iaculis.
-                Cras et sem vel nibh molestie posuere sed sed justo.
-                Mauris justo libero, egestas sed efficitur at, molestie ut risus.
-                Sed eu tristique sapien.
-                Etiam erat purus, ultrices ac feugiat pretium, molestie et erat.
-                Sed vulputate augue ante, in pulvinar tortor malesuada sagittis.
-                Integer rutrum pharetra nisl, vel accumsan velit.
-                Nulla sed bibendum ante.
-                Fusce vel maximus leo.
+                I'm always open to commissions, suggestions or anything else.
+                The best way to reach me is through my&nbsp;
+                <a href='https://www.instagram.com/jonasdrawscats' target='_blank' rel='noopener noreferrer'>Instagram</a>
+                &nbsp;— feel free to drop me a message :)
             </p>
             <p>
-                Maecenas vitae volutpat dui, sit amet vulputate sem.
-                Vivamus odio eros, hendrerit quis mauris eu, varius congue odio.
-                Fusce quis laoreet mauris. Curabitur semper justo a eleifend sollicitudin.
-                Nullam vel tellus a ligula lobortis ornare a at purus.
-                Maecenas eget urna enim.
-                Etiam tincidunt pellentesque ipsum, id porttitor quam convallis sit amet.
-                Pellentesque at quam vel sapien consectetur commodo.
-                Quisque efficitur ultrices felis, fermentum dictum lorem finibus in.
-                Morbi sit amet quam in urna ornare dictum eget id enim.
+                I also built this website myself! If you're into the style and want something similar for your own project, hit me up.
             </p>
         </div>
+
     );
 }
 
